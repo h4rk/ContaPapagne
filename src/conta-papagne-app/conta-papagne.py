f0a56d7f9e8f4485931c234ef275f7fb426de14d
h4rk <ale.sala.97@gmail.com>
2024-01-23 12:57:42 +0100

    Refactor project structure for MVC pattern, setup DB

--- src/conta-papagne-app/conta-papagne.py
+++ src/conta-papagne-app/conta-papagne.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
-from blueprints.movimenti import mov
-from blueprints.investimenti import inv
-from blueprints.budgets import bud
-from blueprints.settings import set
+from controllers.movimenti import mov
+from controllers.investimenti import inv
+from controllers.budgets import bud
+from controllers.settings import set
+from models.dbconfig import db
 
+#Create app
 app = Flask(__name__)
+
+#Setup DB
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+db.init_app(app)
+
+#Register blueprints
 app.register_blueprint(mov)
 app.register_blueprint(inv)
 app.register_blueprint(bud)
