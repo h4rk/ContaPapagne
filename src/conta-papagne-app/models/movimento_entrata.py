0f2853cb9fd37f4567a053573fbcd69fdf746bda
h4rk <ale.sala.97@gmail.com>
2024-01-23 16:37:31 +0100

    set autoincrement id, repr e str

--- src/conta-papagne-app/models/movimento_entrata.py
+++ src/conta-papagne-app/models/movimento_entrata.py
@@ -5,8 +5,14 @@ from models.dbconfig import db
 class MovimentoEntrata(db.Model):
 	__tablename__ = 'movimento_entrata'
 	
-	id_entrata = mapped_column(Integer, primary_key=True)
+	id_entrata = mapped_column(Integer, primary_key=True, autoincrement=True)
 	data = mapped_column(Date)
 	importo = mapped_column(Double)
 	descrizione = mapped_column(String, nullable=True)
 	risarcimento = mapped_column(ForeignKey('movimento_uscita.id_uscita'), nullable=True)
+
+	def __str__(self):
+		return 'MovimentoEntrata(id='+ str(self.id_entrata) +', importo= '+ str(self.importo) +', data='+str(self.data)+')'
+
+	def __repr__(self):
+		return 'MovimentoEntrata(id='+ str(self.id_entrata) +', importo= '+ str(self.importo) +', data='+str(self.data)+')'
--- src/conta-papagne-app/models/movimento_uscita.py
+++ src/conta-papagne-app/models/movimento_uscita.py
@@ -5,7 +5,13 @@ from models.dbconfig import db
 class MovimentoUscita(db.Model):
 	__tablename__ = 'movimento_uscita'
 	
-	id_uscita = mapped_column(Integer, primary_key=True)
+	id_uscita = mapped_column(Integer, primary_key=True, autoincrement=True)
 	data = mapped_column(Date)
 	importo = mapped_column(Double)
 	descrizione = mapped_column(String, nullable=True)
+
+	def __str__(self):
+		return 'MovimentoUscita(id='+ str(self.id_uscita) +', importo= '+ str(self.importo) +', data='+str(self.data)+')'
+
+	def __repr__(self):
+		return 'MovimentoUscita(id='+ str(self.id_uscita) +', importo= '+ str(self.importo) +', data='+str(self.data)+')'
