8e630a01f483a1de355df1bd8d3180db9f3d9ed7
h4rk <ale.sala.97@gmail.com>
2024-01-23 16:37:56 +0100

    test creazione tabelle e righe

--- src/conta-papagne-app/conta-papagne.py
+++ src/conta-papagne-app/conta-papagne.py
@@ -6,6 +6,7 @@ from controllers.settings import set
 from models.dbconfig import db
 from models.movimento_uscita import MovimentoUscita
 from models.movimento_entrata import MovimentoEntrata
+import datetime
 
 #Create app
 app = Flask(__name__)
@@ -13,8 +14,12 @@ app = Flask(__name__)
 #Setup DB
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 db.init_app(app)
-with app.app_context():
+""" with app.app_context():
     db.create_all()
+    db.session.add(MovimentoEntrata(data=datetime.date(2024, 1, 23), importo=123.45, descrizione=None, risarcimento=None))
+    db.session.commit()
+    for q in db.session.execute(db.select(MovimentoEntrata)):
+        print(q) """
 
 #Register blueprints
 app.register_blueprint(mov)
