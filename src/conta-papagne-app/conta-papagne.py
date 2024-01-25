7a1a35e045393a2064c7e38901dbdb441a2bb11b
h4rk <ale.sala.97@gmail.com>
2024-01-25 19:36:53 +0100

    autism

--- src/conta-papagne-app/conta-papagne.py
+++ src/conta-papagne-app/conta-papagne.py
@@ -14,12 +14,12 @@ app = Flask(__name__)
 #Setup DB
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 db.init_app(app)
-""" with app.app_context():
-    db.create_all()
-    db.session.add(MovimentoEntrata(data=datetime.date(2024, 1, 23), importo=123.45, descrizione=None, risarcimento=None))
-    db.session.commit()
-    for q in db.session.execute(db.select(MovimentoEntrata)):
-        print(q) """
+# with app.app_context():
+#     db.create_all()
+#     db.session.add(MovimentoEntrata(data=datetime.date(2024, 1, 23), importo=123.45, descrizione=None, risarcimento=None))
+#     db.session.commit()
+#     for q in db.session.execute(db.select(MovimentoEntrata)):
+#         print(q)
 
 #Register blueprints
 app.register_blueprint(mov)
