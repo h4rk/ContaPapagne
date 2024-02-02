bc3c8a17bb69933401566e0f481f5c80f7e64ca1
h4rk <ale.sala.97@gmail.com>
2024-02-02 18:08:37 +0100

    controller per settings

--- src/ContaPapagneApp/controllers/settings.py
+++ src/ContaPapagneApp/controllers/settings.py
@@ -1,9 +1,21 @@
-from flask import Blueprint, render_template
-import time
+from flask import Blueprint, render_template, request
+from models.categoria import Categoria
+from .repositories import categorieRepository as catRepo
 
 set = Blueprint('settings', __name__, url_prefix='/settings')
 
 @set.route('/', methods=['GET'])
 def dashboard():
-	time.sleep(3)
 	return render_template('set/settings.html')
+
+@set.route('/createCategoria',  methods=['POST'])
+def createCategoria():
+	cat = Categoria(nome=request.form.get('nome_categoria'), descrizione=request.form.get('descrizione_categoria'), tipologia=int(request.form.get('tipologia_categoria')))
+	result = catRepo.createCategoria(cat)
+	return '';
+
+@set.route('/listCategorie',  methods=['GET'])
+def listCategorie():
+	categorie = catRepo.listCategorie()
+	print(categorie)
+	return render_template('set/listaCategorie.html', categorie=categorie);
