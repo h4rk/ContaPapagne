c2764813718683bd2f3a82bd715e72b48a5eefeb
h4rk <ale.sala.97@gmail.com>
2024-02-06 17:01:21 +0100

    refactor con logger, nuovo metodo fetch

--- src/ContaPapagneApp/controllers/repositories/categorieRepository.py
+++ src/ContaPapagneApp/controllers/repositories/categorieRepository.py
@@ -1,17 +1,21 @@
 from models.dbconfig import db
-from sqlalchemy import select
+from sqlalchemy import select, text
 from models.categoria import Categoria
+from flask import current_app as app
 
 def createCategoria(categoria: Categoria) -> int:
-	db.session.add(categoria)	
+	db.session.add(categoria)
 	db.session.commit()
 	return categoria.id_categoria
 
 def listCategorie():
 	stmt = select(Categoria)
-	print(stmt)
+	app.logger.debug("listCategorie query: \n" + str(stmt))
 	res = db.session.scalars(stmt).all()
-	for r in res:
-		print(r)
 	return res
 
+def findCategoriaWithNomeLike(hint):
+	hint = f'%{hint}%'
+	stmt =text("SELECT * FROM categoria c WHERE c.nome LIKE :hint")
+	res = db.session.execute(stmt, {'hint': hint}).all()
+	return res
