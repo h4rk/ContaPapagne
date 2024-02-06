1baa03d0acba5f759a366e6473bba04b82da2b37
h4rk <ale.sala.97@gmail.com>
2024-02-06 17:01:03 +0100

    refactor con logger

--- src/ContaPapagneApp/controllers/settings.py
+++ src/ContaPapagneApp/controllers/settings.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, current_app as app
 from models.categoria import Categoria
 from .repositories import categorieRepository as catRepo
 
@@ -11,11 +11,12 @@ def dashboard():
 @set.route('/createCategoria',  methods=['POST'])
 def createCategoria():
 	cat = Categoria(nome=request.form.get('nome_categoria'), descrizione=request.form.get('descrizione_categoria'), tipologia=int(request.form.get('tipologia_categoria')))
+	app.logger.info("Categoria da persistere: " + str(cat))
 	result = catRepo.createCategoria(cat)
 	return '';
 
 @set.route('/listCategorie',  methods=['GET'])
 def listCategorie():
 	categorie = catRepo.listCategorie()
-	print(categorie)
+	app.logger.info("Categorie recuperate: \n" + str(categorie))
 	return render_template('set/listaCategorie.html', categorie=categorie);
