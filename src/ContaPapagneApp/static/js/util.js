d36eca5c7e2e714622397ed167df8dd594e9a197
h4rk <ale.sala.97@gmail.com>
2024-02-12 17:10:05 +0100

    Slider switch tipo movimento

--- src/ContaPapagneApp/static/js/util.js
+++ src/ContaPapagneApp/static/js/util.js
@@ -1,19 +1,31 @@
 //Funzione di callback da passare come parametro all'event listener dello switch
 // per passare da form entrate a form uscite
-switchForm = function switchForm() {
-	var formContainer = document.getElementById('form-container');
-	var formTitle = document.getElementById('form-title');
-	var submitBtn = document.getElementById('submit-btn');
+// switchForm = function switchForm() {
+// 	var formContainer = document.getElementById('form-container');
+// 	var formTitle = document.getElementById('form-title');
+// 	var submitBtn = document.getElementById('submit-btn');
 
-	if(this.checked) {
-		formContainer.className = 'form-container income-form';
-		formTitle.innerText = 'Income Form';
-		submitBtn.className = 'submit-btn income-btn';
-		submitBtn.innerText = 'Add Income';
+// 	if(this.checked) {
+// 		formContainer.className = 'form-container income-form';
+// 		formTitle.innerText = 'Income Form';
+// 		submitBtn.className = 'submit-btn income-btn';
+// 		submitBtn.innerText = 'Add Income';
+// 	} else {
+// 		formContainer.className = 'form-container expenses-form';
+// 		formTitle.innerText = 'Expenses Form';
+// 		submitBtn.className = 'submit-btn expenses-btn';
+// 		submitBtn.innerText = 'Add Expense';
+// 	}
+// }
+
+function switchTipoMovimento() {
+	// Recupera il valore dello switch per la tipologia di movimento
+	// true = entrata
+	// false = uscita
+	let type = document.getElementById("tipologia_movimento");
+	if (type.checked) {
+		//applicare stile entrata al form
 	} else {
-		formContainer.className = 'form-container expenses-form';
-		formTitle.innerText = 'Expenses Form';
-		submitBtn.className = 'submit-btn expenses-btn';
-		submitBtn.innerText = 'Add Expense';
+		//applicare stile uscita al form
 	}
 }
