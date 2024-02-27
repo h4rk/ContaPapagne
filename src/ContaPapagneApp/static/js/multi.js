995fc48e695cf013e73b20ac0925d038a3d26444
h4rk <ale.sala.97@gmail.com>
2024-02-27 12:21:02 +0100

    Implementazione multiSelect pronta per beta testing

--- src/ContaPapagneApp/static/js/multi.js
+++ src/ContaPapagneApp/static/js/multi.js
@@ -6,7 +6,6 @@ function getDeleteSpan() {
 }
 
 function addCategoryToMulti(e) {
-	console.log('addCategoryToMulti' + e.target.value);
 	//Recupero il multiBox, ovvero l'elemento che contiene graficamente le categorie
 	const box = document.getElementById("multiBox");
 
@@ -15,42 +14,66 @@ function addCategoryToMulti(e) {
 	const input = document.getElementById("categorie_movimento");
 
 	//Recupero la sorgente dell'evento, ovvero il div che contiene la categoria da aggiungere
-	// nel multiBox e in categorie_movimento e 
-	//creo il nuovo elemento da inserire nel multiBox
+	// nel multiBox e in categorie_movimento. Creo un clone, rimuovo l'attributo onclick
+	// e aggiungo un eventListener per il click di rimozione
 	let dx = e.target.cloneNode(true);
-	dx.appendChild(getDeleteSpan());
+	//dx.appendChild(getDeleteSpan()); //TODO Ragionare uslla X per rimuovere
+	dx.removeAttribute('onclick');
+	dx.id = dx.id+'_box'
 	dx.addEventListener('click', removeCategoryFromMulti);
 
 	//Creo il nuovo elemento option da inserire nel select
 	let ix = document.createElement('option');
 	ix.setAttribute('value', e.target.getAttribute('id'));
 	ix.setAttribute('id',e.target.getAttribute('id')+'_opt');
+	ix.setAttribute('selected', 'selected');
 
 	//Aggiungo la nuova categoria al multiBox
 	box.appendChild(dx);
 	//Aggiungo la nuova categoria al categorie_movimento
 	input.appendChild(ix);
 	//Disabilito il result clickato per non permettere doppioni
-	// TODO rimuovere eventHandler? 
-	e.target.setAttribute('disabled', true);
-	e.target.removeAttribute('onclick');
+	e.target.classList.add('hidden');
 }
 
 function removeCategoryFromMulti(e) {
-	//Recupero il multiDiv, ovvero l'elemento che contiene graficamente le categorie
-	const div = document.getElementById("multiBox");
+	//Recupero il multiBox, ovvero l'elemento che contiene graficamente le categorie
+	const box = document.getElementById("multiBox");
 
 	//Recupero il multiInput, ovvero l'elemento select che contiene le options selezionate
 	// ossia quelle che si trovano in ogni momento nel multiDiv
 	const input = document.getElementById("categorie_movimento");
 
-	//Recupero la sorgente dell'evento, ovvero il div che contiene la categoria da aggiungere
-	// nel multiDiv e nel multiInput
-	const category = e.target;
-
-	//Rimuovo la categoria dal multiDiv
-	div.removeChild(div.getElementById(category.id));
+	//Riporto visibile l'elemento nella dropdown
+	let tempId = e.target.id.replace('_box', '');
+	let dd = document.getElementById(tempId);
+	if (dd !== null) {
+		dd.classList.remove('hidden');
+	}
 	//Rimuovo l'option corrispondente dalla select
-	input.removeChild(input.getElementById(category.id));
-	//TODO Riattivare event sulla dropdown se esiste id
+	input.removeChild(document.getElementById(tempId+'_opt'));
+	
+	//Rimuovo la categoria dal multiBox
+	box.removeChild(document.getElementById(e.target.getAttribute('id')));
 }
+
+window.addEventListener("load", function() {
+	document.body.addEventListener("htmx:afterSettle", function(afterSwapEvent) {
+		if(afterSwapEvent.target.id=='multiDropdown') {
+			console.log('Check alredy selected:');
+
+			let listSel = document.getElementById('multiBox').children;
+			let listRes = document.getElementById('multiDropdown').children;
+
+			for(let sel of listSel) {
+				for(let res of listRes) {
+					if(sel.id.replace('_box', '') == res.id) {
+						//res.classList.add('hidden');
+						document.getElementById(res.id).classList.add('hidden');
+						console.log('Adding hidden to ' + res.id);
+					}
+				}
+			}
+		}
+	})
+})
