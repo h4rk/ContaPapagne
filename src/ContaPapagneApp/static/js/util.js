cbfcbc3b20937bf344527161107d0c826df4fafa
h4rk <ale.sala.97@gmail.com>
2024-02-27 23:14:34 +0100

    Formattato html form

--- src/ContaPapagneApp/static/js/util.js
+++ src/ContaPapagneApp/static/js/util.js
@@ -11,3 +11,39 @@ function switchTipoMovimento() {
 		document.getElementById('risarcimento_movimento').setAttribute('disabled', '');
 	}
 }
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+		return new bootstrap.Tooltip(tooltipTriggerEl)
+	})
+
+	function toggleTooltipVisibility() {
+		var tooltipContainer = document.getElementById("tooltip-container");
+
+		if (tooltipContainer.classList.contains("hide")) {
+			tooltipContainer.classList.remove("hide");
+		} else {
+			tooltipContainer.classList.add("hide");
+		}
+	}
+
+	function boldTipologia() {
+		var uscita = document.getElementById("uscita");
+		var entrata = document.getElementById("entrata");
+		var checkbox = document.getElementById("tipologia_movimento");
+
+		if (checkbox.checked) {
+			checkbox.classList.remove("highlight-checkbox")
+			entrata.classList.add("highlight");
+			entrata.classList.remove("hide");
+			uscita.classList.remove("highlight");
+			uscita.classList.add("hide");
+
+		} else {
+			checkbox.classList.add("highlight-checkbox")
+			uscita.classList.add("highlight");
+			uscita.classList.remove("hide");
+			entrata.classList.remove("highlight");
+			entrata.classList.add("hide");
+		}
+	}
