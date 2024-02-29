4f93053acd68d3e487ddbf12fd25dfab7bc31684
h4rk <ale.sala.97@gmail.com>
2024-02-29 15:47:06 +0100

    Cancellazione categorie al switch del form

--- src/ContaPapagneApp/static/js/util.js
+++ src/ContaPapagneApp/static/js/util.js
@@ -12,38 +12,52 @@ function switchTipoMovimento() {
 	}
 }
 
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-		return new bootstrap.Tooltip(tooltipTriggerEl)
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+		return new bootstrap.Tooltip(tooltipTriggerEl);
 	})
 
-	function toggleTooltipVisibility() {
-		var tooltipContainer = document.getElementById("tooltip-container");
+function toggleTooltipVisibility() {
+	var tooltipContainer = document.getElementById("tooltip-container");
 
-		if (tooltipContainer.classList.contains("hide")) {
-			tooltipContainer.classList.remove("hide");
-		} else {
-			tooltipContainer.classList.add("hide");
-		}
+	if (tooltipContainer.classList.contains("hide")) {
+		tooltipContainer.classList.remove("hide");
+	} else {
+		tooltipContainer.classList.add("hide");
 	}
+}
 
-	function boldTipologia() {
-		var uscita = document.getElementById("uscita");
-		var entrata = document.getElementById("entrata");
-		var checkbox = document.getElementById("tipologia_movimento");
+function boldTipologia() {
+	var uscita = document.getElementById("uscita");
+	var entrata = document.getElementById("entrata");
+	var checkbox = document.getElementById("tipologia_movimento");
 
-		if (checkbox.checked) {
-			checkbox.classList.remove("highlight-checkbox")
-			entrata.classList.add("highlight");
-			entrata.classList.remove("hide");
-			uscita.classList.remove("highlight");
-			uscita.classList.add("hide");
+	if (checkbox.checked) {
+		checkbox.classList.remove("highlight-checkbox")
+		entrata.classList.add("highlight");
+		entrata.classList.remove("hide");
+		uscita.classList.remove("highlight");
+		uscita.classList.add("hide");
 
-		} else {
-			checkbox.classList.add("highlight-checkbox")
-			uscita.classList.add("highlight");
-			uscita.classList.remove("hide");
-			entrata.classList.remove("highlight");
-			entrata.classList.add("hide");
-		}
+	} else {
+		checkbox.classList.add("highlight-checkbox")
+		uscita.classList.add("highlight");
+		uscita.classList.remove("hide");
+		entrata.classList.remove("highlight");
+		entrata.classList.add("hide");
 	}
+}
+
+function cleanCategorieAndDropdown() {
+	document.getElementById('multiDropdown').textContent='';
+	document.getElementById('multiBox').textContent='';
+	document.getElementById('multiRicerca').value='';
+	document.getElementById('categorie_movimento').textContent='';
+}
+
+function formTypeSwitchWrapper(e) {
+	switchTipoMovimento();
+	toggleTooltipVisibility();
+	boldTipologia();
+	//cleanCategorieAndDropdown();
+}
